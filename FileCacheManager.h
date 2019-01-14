7de2149a42b54296d0b8f1431d2cf5f8fb2b9231
ShiraNegbi <Shiran96@gmail.com>
2019-01-14 10:30:33 +0200

    Implementing FileCacheManager. Updating FileCacheManager and CacheManager to be templates.

--- FileCacheManager.h
+++ FileCacheManager.h
@@ -4,10 +4,23 @@
 
 #ifndef MILESTONE2_FILECACHEMANAGER_H
 #define MILESTONE2_FILECACHEMANAGER_H
+#include <string>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
 #include "CacheManager.h"
+using namespace std;
 
+template <class Problem, class Solution>
 class FileCacheManager : public CacheManager {
-
+    string fileName;
+    unordered_map<Problem, Solution> solved;
+    void loadFromFile();
+public:
+    FileCacheManager(string fileName);
+    virtual bool hasSolution(Problem problem);
+    virtual Solution getSolution(Problem problem);
+    virtual void saveSolution(Problem problem, Solution solution);
 };
 
 #endif //MILESTONE2_FILECACHEMANAGER_H
